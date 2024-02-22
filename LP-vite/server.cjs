c92b21fead4b2a3de800385732262aea96f8732d
Szymon Kowal <00szymon.kowal@gmail.com>
2024-02-22 09:54:30 +0100

    Add folder with vite config

---
+++ LP-vite/server.cjs
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 3001;
+
+// Serve static files from 'dist' directory
+app.use(express.static('dist'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
