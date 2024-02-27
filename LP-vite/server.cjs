226dd603e30525b2f7facf2922106a071fe08bfa
omichalo121 <michal.basisty@wp.pl>
2024-02-27 14:15:24 +0100

    idk some random shit

--- LP-vite/server.cjs
+++ LP-vite/server.cjs
@@ -1,16 +1,16 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
 const app = express();
 const port = process.env.PORT || 3001;
 
 // Serve static files from 'dist' directory
-app.use(express.static('dist'));
+app.use(express.static("dist"));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "dist", "index.html"));
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+    console.log(`Example app listening at http://localhost:${port}`);
 });
